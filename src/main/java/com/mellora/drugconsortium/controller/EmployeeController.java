4f3aa0ebfa24cbb0ea762366ecdf5a7f4a92327f
Alexander Mellor <AlexMellor2014@gmail.com>
2020-03-30 13:35:21 -0500

    Added delete employee functionality

--- src/main/java/com/mellora/drugconsortium/controller/EmployeeController.java
+++ src/main/java/com/mellora/drugconsortium/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,12 @@ public class EmployeeController {
 	@PostMapping(path = "/employee/add")
 	public Employee addEmployee(Employee employee) {
 		eRepo.save(employee);
+		return employee;
+	}
+	
+	@DeleteMapping(path = "/employee/remove")
+	public Employee removeEmployee(Employee employee) {
+		eRepo.delete(employee);
 		return employee;
 	}
 }
