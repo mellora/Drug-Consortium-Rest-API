c96fdcfca36f4d9f77a25323dbe4b53c0fba71a9
Alexander Mellor <AlexMellor2014@gmail.com>
2020-03-31 10:18:46 -0500

    Modified finding employee by name to be able to return null

--- src/main/java/com/mellora/drugconsortium/dao/EmployeeRepo.java
+++ src/main/java/com/mellora/drugconsortium/dao/EmployeeRepo.java
@@ -1,5 +1,7 @@
 package com.mellora.drugconsortium.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,5 +10,5 @@ import com.mellora.drugconsortium.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 	@Query(value = "SELECT * FROM employee e WHERE e.FIRST_NAME = :first AND e.LAST_NAME = :last", nativeQuery = true)
-	public Employee findEmployeeByFirstAndLastName(@Param("first") String firstName, @Param("last") String lastName);
+	public Optional<Employee> findEmployeeByFirstAndLastName(@Param("first") String firstName, @Param("last") String lastName);
 }
