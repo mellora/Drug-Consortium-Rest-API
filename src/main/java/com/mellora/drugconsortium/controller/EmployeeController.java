e419be2f8346811254c6426504318101572d6967
Alexander Mellor <AlexMellor2014@gmail.com>
2020-03-31 17:39:05 -0500

    Implemented employee update functionality

--- src/main/java/com/mellora/drugconsortium/controller/EmployeeController.java
+++ src/main/java/com/mellora/drugconsortium/controller/EmployeeController.java
@@ -6,9 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,8 +65,9 @@ public class EmployeeController {
 		return employeeList;
 	}
 
-	@PatchMapping(path = "/employee/update")
-	public Employee updateEmployee(Employee employee) {
+	@PutMapping(path = "/employee/update")
+	public Employee saveOrUpdateEmployee(@RequestBody Employee employee) {
+		eRepo.save(employee);
 		return employee;
 	}
 }
