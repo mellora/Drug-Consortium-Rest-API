9716f741afbec078300558a292d1c6c6bba03ae0
Alexander Mellor <AlexMellor2014@gmail.com>
2020-03-31 10:18:06 -0500

    Switched repository primary key from Integer to Long

--- src/main/java/com/mellora/drugconsortium/dao/EmployeeRepo.java
+++ src/main/java/com/mellora/drugconsortium/dao/EmployeeRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.mellora.drugconsortium.model.Employee;
 
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 	@Query(value = "SELECT * FROM employee e WHERE e.FIRST_NAME = :first AND e.LAST_NAME = :last", nativeQuery = true)
 	public Employee findEmployeeByFirstAndLastName(@Param("first") String firstName, @Param("last") String lastName);
 }
